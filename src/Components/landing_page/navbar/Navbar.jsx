15609792c2935f3e8a6ba089db25aabea2305590
mRizky09 <mrizky942003@gmail.com>
2023-11-18 19:32:42 +0700

    update eror

--- src/Components/landing_page/navbar/Navbar.jsx
+++ src/Components/landing_page/navbar/Navbar.jsx
@@ -11,67 +11,31 @@ const NavbarHome = () => {
   const linkHome = useNavigate();
   const linkLogin = useNavigate();
   const linkDaftar = useNavigate();
-    const linkKonsultasiPage = useNavigate();
+  const linkKonsultasiPage = useNavigate();
+
   return (
-<<<<<<< HEAD
     <div className='h_main_navbar'>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">
-            <img src={logo} alt="logoaplikasi" style={{width:"100%", height:"100%", objectFit:'cover'}}/>
+            <img src={logo} alt="logoaplikasi" style={{ width: "100%", height: "100%", objectFit: 'cover' }} />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto text-center d-flex justify-content-center align-items-center">
-              <Nav.Link href=""onClick={()=> linkHome("/")} className='text-secondary fw-bold'>Home</Nav.Link>
-              <Nav.Link href="" onClick={()=> linkKonsultasiPage("/konsultasi_page")} className='text-secondary fw-bold'>Konsultasi</Nav.Link>
+              <Nav.Link href="" onClick={() => linkHome("/")} className='text-secondary fw-bold'>Home</Nav.Link>
+              <Nav.Link href="" onClick={() => linkKonsultasiPage("/konsultasi_page")} className='text-secondary fw-bold'>Konsultasi</Nav.Link>
               <Nav.Link href="#" className='text-secondary fw-bold'>Layanan Kesehatan</Nav.Link>
               <Nav.Link href="#" className='text-secondary fw-bold'>Artikel</Nav.Link>
-              <Nav.Link href=""onClick={() => linkLogin("/login")}  className='text-black fw-bold'>Masuk</Nav.Link>
+              <Nav.Link href="" onClick={() => linkLogin("/login")} className='text-black fw-bold'>Masuk</Nav.Link>
               <Nav.Link href="#home" className='text-white fw-bold text-decoration-none'>
-                <Button onClick={()=> linkDaftar ('/daftar')} className='bg_login'>Daftar</Button>
+                <Button onClick={() => linkDaftar('/daftar')} className='bg_login'>Daftar</Button>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
-=======
-      <div className="main_navbar">
-        <Navbar expand="lg" className="bg-body-tertiary position-fixed w-100">
-          <Container>
-            <Navbar.Brand href="#home">
-              <img src={logo} alt="logoaplikasi" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto text-center d-flex justify-content-center align-items-center">
-                <Nav.Link href="#home" className="text-secondary fw-bold page">
-                  Home
-                </Nav.Link>
-                <Nav.Link href="#" className="text-secondary fw-bold page">
-                  Konsultasi
-                </Nav.Link>
-                <Nav.Link href="#" className="text-secondary fw-bold page">
-                  Layanan Kesehatan
-                </Nav.Link>
-                <Nav.Link href="#" className="text-secondary fw-bold page">
-                  Artikel
-                </Nav.Link>
-                <Nav.Link href="" onClick={() => linkLogin('/login')} className="text-black fw-bold">
-                  Masuk
-                </Nav.Link>
-                <Nav.Link href="#home" className="text-white fw-bold text-decoration-none">
-                  <Button onClick={() => linkDaftar('/daftar')} className="bg_login">
-                    Daftar
-                  </Button>
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
->>>>>>> 572808c36d923b86e7d770226b16e2aa3bf5a9f8
   );
 };
 
