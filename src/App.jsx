ce12fde2b394e9e1d3deefe103df54de1eca9857
muradha <mhdrizki098@gmail.com>
2023-11-19 20:46:42 +0700

    `add` : layanan kesehatan

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./Components/Login_daftar/Login.jsx";
 import Daftar from "./Components/Login_daftar/Daftar.jsx";
 import Konsultasi_page from "./Components/konsultasi_page/Konsultasi_page.jsx";
 import HeaderMain from "./Components/landing_page/headermain_landingpage/HeaderMain.jsx";
+import Index from "./Components/layanan_kesehatan/Index.jsx";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HeaderMain />} />
           <Route path="/konsultasi_page" element={<Konsultasi_page />} />
+          <Route path="/layanan-kesehatan" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Daftar" element={<Daftar />} />
         </Routes>
