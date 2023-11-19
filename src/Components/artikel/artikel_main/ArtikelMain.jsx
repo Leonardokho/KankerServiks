116458f2705d228f13c3ab3da8c7251e60f37cf5
Leonardo Kho <kholeonardo100@gmail.com>
2023-11-19 21:11:11 +0700

    update artikel

--- src/Components/artikel/artikel_main/ArtikelMain.jsx
+++ src/Components/artikel/artikel_main/ArtikelMain.jsx
@@ -1,14 +1,12 @@
 import Footer from "../../landing_page/footer/Footer"
-// import NavbarHome from "../../landing_page/navbar/Navbar"
+import NavbarHome from "../../landing_page/navbar/Navbar"
 import IsiArtikel from "../isi_artikel/IsiArtikel"
-import NavbarArtikel from "../navbar_artikel/NavbarArtikel"
 import SearchArtikel from "../search_artikel/SearchArtikel"
 
 const ArtikelMain = () => {
   return (
     <div>
-        <NavbarArtikel/>
-        {/* <NavbarHome/> */}
+        <NavbarHome/>
         <SearchArtikel/>
         <IsiArtikel/>
         <Footer/>
