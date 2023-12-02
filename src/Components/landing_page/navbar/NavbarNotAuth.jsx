d5098eec700ccb124dfb14912d4bcc11dda61d89
Leonardo Kho <kholeonardo100@gmail.com>
2023-12-03 00:50:07 +0700

    update navbar

--- src/Components/landing_page/navbar/NavbarNotAuth.jsx
+++ src/Components/landing_page/navbar/NavbarNotAuth.jsx
@@ -16,34 +16,38 @@ const NavbarNotAuth = () => {
 
   return (
     <div className="navbar_main">
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg_color_navbar">
         <Container>
           <Navbar.Brand href="/">
             <img src={logo} alt="logoaplikasi" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" >
             <Nav className="ms-auto text-center d-flex justify-content-center align-items-center nav_text">
-              <Nav.Link onClick={() => linkHome('/')} className="text-secondary fw-bold nav_text_change">
-                Home
-              </Nav.Link>
-              <Nav.Link onClick={() => linkKonsultasiPage('/Halaman_Konsultasi')} className="text-secondary fw-bold nav_text_change">
-                Konsultasi
-              </Nav.Link>
-              <Nav.Link onClick={() => linkLayananKesehatan('/Layanan_Kesehatan')} className="text-secondary fw-bold nav_text_change">
-                Layanan Kesehatan
-              </Nav.Link>
-              <Nav.Link onClick={() => linkArtikelPage('/Halaman_Artikel')} className="text-secondary fw-bold nav_text_change">
-                Artikel
-              </Nav.Link>
-              <Nav.Link onClick={() => linkLogin('/Masuk')} className="text-black fw-bold nav_text_change">
-                Masuk
-              </Nav.Link>
-              <Nav.Link className="text-white fw-bold text-decoration-none">
-                <Button onClick={() => linkDaftar('/Daftar')} className="bg_login">
-                  Daftar
-                </Button>
-              </Nav.Link>
+              <div className='page_navbar'>
+                <Nav.Link onClick={() => linkHome('/')} className="text-secondary fw-bold nav_text_change">
+                  Home
+                </Nav.Link>
+                <Nav.Link onClick={() => linkKonsultasiPage('/Halaman_Konsultasi')} className="text-secondary fw-bold nav_text_change">
+                  Konsultasi
+                </Nav.Link>
+                <Nav.Link onClick={() => linkLayananKesehatan('/Layanan_Kesehatan')} className="text-secondary fw-bold nav_text_change">
+                  Layanan Kesehatan
+                </Nav.Link>
+                <Nav.Link onClick={() => linkArtikelPage('/Halaman_Artikel')} className="text-secondary fw-bold nav_text_change">
+                  Artikel
+                </Nav.Link>
+              </div>
+              <div className="login_button">
+                <Nav.Link onClick={() => linkLogin('/Masuk')} className="text-black fw-bold nav_text_change">
+                  Masuk
+                </Nav.Link>
+                <Nav.Link className="text-white fw-bold text-decoration-none">
+                  <Button onClick={() => linkDaftar('/Daftar')} className="bg_login">
+                    Daftar
+                  </Button>
+                </Nav.Link>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
